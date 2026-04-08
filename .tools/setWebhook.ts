c97984549d004214cf2c3c117655e9feb1ec4f21
angel <angel.yarkiy@gmail.com>
2026-04-08 17:16:42 +0500

    +

--- .tools/setWebhook.ts
+++ .tools/setWebhook.ts
@@ -1,51 +1,37 @@
 import { Telegraf } from "telegraf";
 
-export type TMode = 'preprod' | 'prod' | 'local' | 'adminBot' | 'adminBot_toLocal' | undefined;
+
+export type TMode = 'ppBot_toLocal'|'preprod' | 'prod' | 'local' | 'adminBot' | 'adminBot_toLocal' | undefined;
+export interface D {
+    BOT_TOKEN: string; 
+    SERVER_URL: string;
+    apiURL: string;
+    title: string;
+}
 export interface WebhookConfig {
     mode: TMode;
-    ngrokUrl: string;
-    apiUrl: string;
-    prod: { BOT_TOKEN: string; API_URL: string; };
-    adminBot: { BOT_TOKEN: string; API_URL: string; };
-    adminBot_toLocal: { BOT_TOKEN: string; API_URL: string; };   
-    
+    data: Record<string, D>  
 }
 
 export async function setWebhook(config: WebhookConfig) {
     const mode = config.mode;
-    const apiUrl = config.apiUrl;
-    let url: string = '';
-    let bot_token: string = '';
-    let resultStr = '';
+    if (!mode) return;
 
-    if (!mode || !['preprod', 'prod', 'local'].includes(mode)) {
-        console.error('❌ Укажите режим: preprod | prod | local');
-        process.exit(1);
-    }
+    
+    const data = config.data[mode];
+    const bot_token = data.BOT_TOKEN;
+    const url = data.SERVER_URL;
+    const apiUrl = data.apiURL;
+    const resultStr = `✅ Установил вебхук для ${data.title}`;
 
-    switch (mode) {
-        case 'preprod':
-            bot_token = config.preprod.BOT_TOKEN;
-            url = config.preprod.API_URL;
-            resultStr = `✅ Установил вебхук для ДЕВ бота\n   ${url}`;
-            break;
-        case 'prod':
-            bot_token = config.prod.BOT_TOKEN;
-            url = config.prod.API_URL;
-            resultStr = `✅ Установил вебхук для ОСНОВНОГО бота\n   ${url}`;
-            break;
-        case 'local':
-            bot_token = config.preprod.BOT_TOKEN;
-            url = `https://${config.ngrokUrl}.ngrok-free.app`;
-            resultStr = `✅ Установил вебхук для ЛОКАЛЬНОГО бота\n   ${url}`;
-            break;
-    }
 
-    try {
+    try {        
         const bot = new Telegraf(bot_token);
         const result = await bot.telegram.setWebhook(url + apiUrl);
-        console.log('Webhook успешно установлен:', result);
         console.log(resultStr);
+        console.log(url + apiUrl);
+        console.log(result);
+
     } catch (e) {
         console.error('Ошибка при настройке бота:', e);
     }
