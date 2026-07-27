0805e27f3d44e64e7ced9d2a387f2969d93aa10f
angel <angel.yarkiy@gmail.com>
2026-07-27 10:23:29 +0500

    изменил setWebhook - может вызвать ошибки в старых версиях

--- _tools/setWebhook.ts
+++ _tools/setWebhook.ts
@@ -1,25 +1,38 @@
 import { Telegraf } from "telegraf";
 
-export type Links = Record<string, D>;
-export type TMode =
-  | "ppBot_toLocal"
-  | "preprod"
-  | "prod"
-  | "local"
-  | "adminBot"
-  | "adminBot_toLocal"
-  | "adminBot_toProd"
-  | undefined;
-export interface D {
+export type Links = Record<string, WebhookConfig>;
+// export type TMode =
+//   | "ppBot_toLocal"
+//   | "preprod"
+//   | "prod"
+//   | "local"
+//   | "adminBot"
+//   | "adminBot_toLocal"
+//   | "adminBot_toProd"
+//   | undefined;
+
+// export const MODE = {
+//   clientDev: "clientDev",
+//   clientProd: "clientProd",
+//   clientPreprod: "clientPreprod",
+//   clientPP: "clientPP",
+//   adminBot: "adminBot",
+//   adminBotDev: "adminBotDev",
+//   adminBotProd: "adminBotProd",
+//   adminBotPreprod: "adminBotPreprod",
+//   adminBotPP: "adminBotPP",
+// } as const;
+export interface WebhookConfig {
   BOT_TOKEN: string;
   SERVER_URL: string;
   apiURL: string;
   title?: string;
+  name?: string;
 }
-export interface WebhookConfig {
-  mode: TMode;
-  data: Record<string, D>;
-}
+// export interface WebhookConfig {
+//   mode: TMode;
+//   data: Record<string, D>;
+// }
 export async function setWebhook2(name: string = "", links: Links) {
   const config = links[name];
   if (!config) return;
@@ -27,6 +40,24 @@ export async function setWebhook2(name: string = "", links: Links) {
   const url = config.SERVER_URL;
   const apiUrl = config.apiURL;
   const resultStr = `✅ ${name}`;
+
+  try {
+    const bot = new Telegraf(bot_token);
+    const result = await bot.telegram.setWebhook(url + apiUrl);
+    console.log(resultStr);
+    console.log(url + apiUrl);
+    console.log(result);
+  } catch (e) {
+    console.error("Ошибка при настройке бота:", e);
+  }
+}
+export async function setWebhook3(config: WebhookConfig) {
+  // const config = links[name];
+  // if (!config) return;
+  const bot_token = config.BOT_TOKEN;
+  const url = config.SERVER_URL;
+  const apiUrl = config.apiURL;
+  const resultStr = `✅ ${config.name || ""}`;
 
   try {
     const bot = new Telegraf(bot_token);
